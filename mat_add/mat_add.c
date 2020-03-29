59df37d017ee8a9c6324c0c325b0b6094f8e8432
valdinijan <valdinijan@gmail.com>
2020-03-29 23:07:38 +0200

    tidy up add and mult

--- mat_add/mat_add.c
+++ mat_add/mat_add.c
@@ -3,15 +3,12 @@
 
 // MAT_DIM must be a multiple of 4 for below code to work
 #define MAT_DIM 64
-#define MAT_1D_LEN ( MAT_DIM * MAT_DIM )
-#define MAT_SIZE ( MAT_1D_LEN * sizeof(unsigned short))
+#define MAT_LEN ( MAT_DIM * MAT_DIM )
+#define MAT_SIZE ( MAT_LEN * sizeof(unsigned short))
 
-static unsigned short l2_mat_buf[MAT_1D_LEN * 3];
-unsigned short * p_l2_mat_1 = &l2_mat_buf[0];
-unsigned short * p_l2_mat_2 = &l2_mat_buf[MAT_1D_LEN];
-unsigned short * p_l2_mat_r = &l2_mat_buf[MAT_1D_LEN * 2];
+static unsigned short l2_mat[3][MAT_LEN];
 
-static unsigned short l2_mat_verif[MAT_1D_LEN];
+static unsigned short l2_mat_verif[MAT_LEN];
 
 #if 0
 static void end_of_l2_to_l1_copy(void *arg)
@@ -31,7 +28,7 @@ static void single_core_work(void *arg)
   unsigned short * p_mat_2 = (unsigned short *)&cluster_l1_buff[mat_2_start];
   unsigned short * p_mat_r = (unsigned short *)&cluster_l1_buff[mat_r_start];
 
-  int chunk_len = MAT_1D_LEN / 8;
+  int chunk_len = MAT_LEN / 8;
   printf("(%d, %d): start_1 %d, start_2 %d\n", rt_cluster_id(), rt_core_id(), mat_1_start, mat_2_start);
 
   for (int offs = 0; offs < chunk_len; offs++)
@@ -52,7 +49,7 @@ static void cluster_entry(void *arg)
 
   // Copy matrices 1 and 2 from L2 to shared L1
   rt_dma_copy_t dmaCp;
-  rt_dma_memcpy((int)l2_mat_buf, (int)cluster_l1_buff, MAT_SIZE * 2, RT_DMA_DIR_EXT2LOC, 0, &dmaCp);
+  rt_dma_memcpy((int)l2_mat, (int)cluster_l1_buff, MAT_SIZE * 2, RT_DMA_DIR_EXT2LOC, 0, &dmaCp);
   // Wait for the operation to finish
   rt_dma_wait(&dmaCp);
 
@@ -60,8 +57,7 @@ static void cluster_entry(void *arg)
   rt_team_fork(8, single_core_work, cluster_l1_buff);
 
   // Copy result matrix from shared L1 to L2
-  rt_dma_memcpy((int)p_l2_mat_r, (int)&cluster_l1_buff[MAT_SIZE * 2],
-                MAT_SIZE, RT_DMA_DIR_LOC2EXT, 0, &dmaCp);
+  rt_dma_memcpy((int)l2_mat[2], (int)&cluster_l1_buff[MAT_SIZE * 2], MAT_SIZE, RT_DMA_DIR_LOC2EXT, 0, &dmaCp);
   // Wait for it to finish
   rt_dma_wait(&dmaCp);
 }
@@ -74,12 +70,12 @@ int main()
   if (rt_event_alloc(NULL, 4)) return -1;
 
   // Initialize matrices in L2 memory
-  for (int i = 0; i < MAT_1D_LEN; i++)
+  for (int i = 0; i < MAT_LEN; i++)
   {
-    p_l2_mat_1[i] = i;
-    p_l2_mat_2[i] = MAT_1D_LEN + i;
-    p_l2_mat_r[i] = 0;
-    l2_mat_verif[i] = i + MAT_1D_LEN + i;
+    l2_mat[0][i] = i;
+    l2_mat[1][i] = MAT_LEN + i;
+    l2_mat[2][i] = 0;
+    l2_mat_verif[i] = i + MAT_LEN + i;
   }
 
   // Power on the cluster
@@ -93,7 +89,7 @@ int main()
   // Copy from L2 to L1
   rt_event_t *p_event = rt_event_get(NULL, end_of_l2_to_l1_copy, (void *)0);
   rt_dma_copy_t dmaCp;
-  rt_dma_memcpy_event((int)l2_mat_buf, (int)cluster_l1_buff, MAT_1D_LEN * 2, RT_DMA_DIR_EXT2LOC, &dmaCp, p_event);
+  rt_dma_memcpy_event((int)l2_mat_buf, (int)cluster_l1_buff, MAT_LEN * 2, RT_DMA_DIR_EXT2LOC, &dmaCp, p_event);
   // Wait for copy to finish
   rt_event_execute(NULL, 1);
 #endif
@@ -110,14 +106,14 @@ int main()
 
   // Verify correct addition
   printf("Verifying...\n");
-  for (int i = 0; i < MAT_1D_LEN; i++)
+  for (int i = 0; i < MAT_LEN; i++)
   {
-    if (p_l2_mat_r[i] != l2_mat_verif[i])
+    if (l2_mat[2][i] != l2_mat_verif[i])
     {
       printf("Test failed: ERROR at index %d: expected %d, actual %d\n",
              i,
              l2_mat_verif[i],
-             p_l2_mat_r[i]);
+             l2_mat[2][i]);
       return -1;
     }
   }
--- mat_mult/mat_mult.c
+++ mat_mult/mat_mult.c
@@ -3,16 +3,12 @@
 
 // MAT_DIM must be a multiple of 8 for below code to work
 #define MAT_DIM 64
-#define MAT_1D_LEN ( MAT_DIM * MAT_DIM )
-#define MAT_SIZE ( MAT_1D_LEN * sizeof(unsigned short))
+#define MAT_LEN ( MAT_DIM * MAT_DIM )
+#define MAT_SIZE ( MAT_LEN * sizeof(unsigned short))
 #define NUM_CORES 8
 
-static unsigned short l2_mat_buf[MAT_1D_LEN * 3];
-unsigned short * p_l2_mat_1 = &l2_mat_buf[0];
-unsigned short * p_l2_mat_2 = &l2_mat_buf[MAT_1D_LEN];
-unsigned short * p_l2_mat_r = &l2_mat_buf[MAT_1D_LEN * 2];
-
-static unsigned short l2_mat_verif[MAT_1D_LEN];
+static unsigned short l2_mat[3][MAT_LEN];
+static unsigned short l2_mat_verif[MAT_LEN];
 
 #if 0
 static void end_of_l2_to_l1_copy(void *arg)
@@ -72,7 +68,7 @@ static void cluster_entry(void *arg)
 
   // Copy from L2 to shared L1
   rt_dma_copy_t dmaCp;
-  rt_dma_memcpy((int)l2_mat_buf, (int)cluster_l1_buff, MAT_SIZE * 3, RT_DMA_DIR_EXT2LOC, 0, &dmaCp);
+  rt_dma_memcpy((int)l2_mat, (int)cluster_l1_buff, MAT_SIZE * 3, RT_DMA_DIR_EXT2LOC, 0, &dmaCp);
   // Wait for the operation to finish
   rt_dma_wait(&dmaCp);
 
@@ -80,7 +76,7 @@ static void cluster_entry(void *arg)
   rt_team_fork(NUM_CORES, single_core_work, cluster_l1_buff);
 
   // Copy result from shared L1 to L2
-  rt_dma_memcpy((int)p_l2_mat_r, (int)&cluster_l1_buff[MAT_SIZE * 2],
+  rt_dma_memcpy((int)l2_mat[2], (int)&cluster_l1_buff[MAT_SIZE * 2],
                 MAT_SIZE, RT_DMA_DIR_LOC2EXT, 0, &dmaCp);
   // Wait for it to finish
   rt_dma_wait(&dmaCp);
@@ -94,12 +90,12 @@ int main()
   if (rt_event_alloc(NULL, 4)) return -1;
 
   // Initialize matrices in L2 memory
-  for (int i = 0; i < MAT_1D_LEN; i++)
+  for (int i = 0; i < MAT_LEN; i++)
   {
-    p_l2_mat_1[i] = i;
-    p_l2_mat_2[i] = 1;
-    p_l2_mat_r[i] = 0;
-    l2_mat_verif[i] = i + MAT_1D_LEN + i;
+    l2_mat[0][i] = i;
+    l2_mat[1][i] = 1;
+    l2_mat[2][i] = 0;
+    l2_mat_verif[i] = i + MAT_LEN + i;
   }
 
   for (int i = 0; i < MAT_DIM; i++)
@@ -107,9 +103,9 @@ int main()
     for (int j = 0; j < MAT_DIM; j++)
     {
       if (i == j)
-        p_l2_mat_2[i * MAT_DIM + j] = 1;
+        l2_mat[1][i * MAT_DIM + j] = 1;
       else
-        p_l2_mat_2[i * MAT_DIM + j] = 0;
+        l2_mat[1][i * MAT_DIM + j] = 0;
     }
   }
 
@@ -124,7 +120,7 @@ int main()
   // Copy from L2 to L1
   rt_event_t *p_event = rt_event_get(NULL, end_of_l2_to_l1_copy, (void *)0);
   rt_dma_copy_t dmaCp;
-  rt_dma_memcpy_event((int)l2_mat_buf, (int)cluster_l1_buff, MAT_1D_LEN * 2, RT_DMA_DIR_EXT2LOC, &dmaCp, p_event);
+  rt_dma_memcpy_event((int)l2_mat_buf, (int)cluster_l1_buff, MAT_LEN * 2, RT_DMA_DIR_EXT2LOC, &dmaCp, p_event);
   // Wait for copy to finish
   rt_event_execute(NULL, 1);
 #endif
@@ -139,12 +135,13 @@ int main()
   rt_cluster_mount(0, 0, 0, NULL);
   printf("Cluster switched off\n");
 
-#if 0
+#if 
+  0
   // Dump result
   printf("Result dump...\n");
-  for (int i = 0; i < MAT_1D_LEN; i++)
+  for (int i = 0; i < MAT_LEN; i++)
   {
-    printf("mat_r[%d]: %d\n", i, p_l2_mat_r[i]);
+    printf("mat_r[%d]: %d\n", i, l2_mat[2][i]);
   }
   printf("OK.\n");
 #endif
